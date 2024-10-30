c259d491332085f23925d633a7033a7a3e95459c
dubeyRahul26 <153608907+dubeyRahul26@users.noreply.github.com>
2024-10-30 10:57:47 +0530

    first part completed - backend

---
+++ backend/db/connectToDB.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectToMongoDB = async (req , res) =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI) ;
+        console.log(`Connected to MongoDB : ${conn.connection.host} : ${conn.connection.port}`);
+        
+    } catch (error) {
+        console.error("Error connecting MongoDB : " ,error);
+    }
+}
+
+export default connectToMongoDB ;
