e0c080811dfc55083eab5dc5a527d5d9c2c0f3aa
dubeyRahul26 <153608907+dubeyRahul26@users.noreply.github.com>
2024-11-01 10:51:38 +0530

    Second part Completed-UI Design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,35 @@
+const Message = () => {
+  return (
+    <div>
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            />
+          </div>
+        </div>
+        <div className="chat-bubble text-white bg-blue-500">
+          Hello
+        </div>
+      </div>
+
+      <div className="chat chat-end">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            />
+          </div>
+        </div>
+        <div className="chat-bubble">
+         Hii
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
