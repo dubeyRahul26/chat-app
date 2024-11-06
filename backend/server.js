ca4cbbf01b51e6dac4b3cf90b72710419a510ec2
dubeyRahul26 <153608907+dubeyRahul26@users.noreply.github.com>
2024-11-06 18:10:25 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,13 @@ import connectToMongoDB from "./db/connectToDB.js";
 import messageRoutes from "./routes/message.routes.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
-import { app ,server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,9 +22,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
 
 server.listen(PORT, (req, res) => {
   connectToMongoDB();
