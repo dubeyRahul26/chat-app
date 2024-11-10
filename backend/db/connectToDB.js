2ec966aeb0d1b8bc58c841a1434aa0071b436863
dubeyRahul26 <153608907+dubeyRahul26@users.noreply.github.com>
2024-11-10 09:42:03 +0530

    URL Updated

--- backend/db/connectToDB.js
+++ backend/db/connectToDB.js
@@ -2,11 +2,12 @@ import mongoose from 'mongoose';
 
 const connectToMongoDB = async (req , res) =>{
     try {
+        
         const conn = await mongoose.connect(process.env.MONGO_URI) ;
         console.log(`Connected to MongoDB : ${conn.connection.host} : ${conn.connection.port}`);
         
     } catch (error) {
-        console.error("Error connecting MongoDB : " ,error);
+        console.error("Error connecting MongoDB : " ,error.message);
     }
 }
 
