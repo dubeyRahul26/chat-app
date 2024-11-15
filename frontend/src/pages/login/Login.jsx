dfa93260ff2b2879b8f4ce0a6066dd1ade67f4ba
dubeyRahul26 <153608907+dubeyRahul26@users.noreply.github.com>
2024-11-15 18:45:58 +0530

    updated

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -64,7 +64,7 @@ const Login = () => {
 
           {/* Submit butoon */}
           <div>
-            <button className="btn btn-block btn-sm mt-2" disabled={loading}>
+            <button className="btn btn-block btn-sm mt-2 border border-slate-700" disabled={loading}>
               {loading ? (
                 <span className="loading loading-spinner "></span>
               ) : (
